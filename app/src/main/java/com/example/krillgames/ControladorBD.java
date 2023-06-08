8adaa5085d374c73cd86cbe1c6669d6af5dd62ec
Noriega20310003 <75447666+Noriega20310003@users.noreply.github.com>
2023-06-07 20:55:15 -0600

    Add files via upload

---
+++ app/src/main/java/com/example/krillgames/ControladorBD.java
@@ -0,0 +1,37 @@
+package com.example.krillgames;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+public class ControladorBD extends SQLiteOpenHelper {
+    public ControladorBD(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
+        super (context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase nombreBD) {
+        String sqlUsuarios = "create table Usuario (IDU INTEGER PRIMARY KEY AUTOINCREMENT, Nombre text, Contrasena text, Correo text, Huella blob)";
+        String sqlVideojuego = "create table Videojuego (IDV INTEGER PRIMARY KEY AUTOINCREMENT, Nombre text, Precio int, Disponibilidad boolean, CodigoQR blob)";
+        String sqlCompras = "create table Compras (IDC INTEGER PRIMARY KEY AUTOINCREMENT, idu INTEGER REFERENCES Usuario(IDU) ON UPDATE CASCADE ON DELETE CASCADE, idv INTEGER REFERENCES Videojuego(IDV) ON UPDATE CASCADE ON DELETE CASCADE)";
+        String sqlSucursal = "create table Sucursal (IDS INTEGER PRIMARY KEY AUTOINCREMENT, Nombre text, Direccion text, Ubicacion text, Telefono int)";
+        String sqlRuta = "create table Ruta (IDR INTEGER PRIMARY KEY AUTOINCREMENT, Distancia int, tiempo int,idu INTEGER REFERENCES Usuario(IDU) ON UPDATE CASCADE ON DELETE CASCADE, ids INTEGER REFERENCES Sucursal(IDS) ON UPDATE CASCADE ON DELETE CASCADE )";
+
+        try{
+            nombreBD.execSQL(sqlUsuarios);
+            nombreBD.execSQL(sqlVideojuego);
+            nombreBD.execSQL(sqlCompras);
+            nombreBD.execSQL(sqlSucursal);
+            nombreBD.execSQL(sqlRuta);
+        }catch (SQLException e){
+            Toast.makeText(null, "Error al crear la base de datos", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+}
+
